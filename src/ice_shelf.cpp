9240cbaf89b21d264d68bf600aa5c1e398d732c4
danshapero <shapero.daniel@gmail.com>
2017-08-29 12:19:54 -0700

    Added search + Newton options

--- src/ice_shelf.cpp
+++ src/ice_shelf.cpp
@@ -5,28 +5,62 @@
 
 namespace icepack
 {
+  template <typename T>
+  void bind_newton_search_options(py::module& module, const std::string& name)
+  {
+    using numerics::LineSearchOptions;
+    using numerics::NewtonSearchOptions;
+    using Callback = typename LineSearchOptions<T>::Callback;
+
+    const std::string callback_name = "Callback" + name;
+    py::class_<Callback>(module, callback_name.c_str());
+
+    const std::string line_search_name = "LineSearchOptions" + name;
+    py::class_<LineSearchOptions<T>>(module, line_search_name.c_str())
+      .def(py::init<const double, const double, const Callback>(),
+           py::arg("armijo") = py::float_(LineSearchOptions<T>::armijo_default),
+           py::arg("wolfe") = py::float_(LineSearchOptions<T>::wolfe_default),
+           py::arg("callback") = Callback([](const double, const T&){}));
+
+    const std::string newton_search_name = "NewtonSearchOptions" + name;
+    py::class_<NewtonSearchOptions<T>>(module, newton_search_name.c_str())
+      .def(py::init<const Callback, const LineSearchOptions<T>>(),
+           py::arg("callback") = Callback([](const double, const T&){}),
+           py::arg("line_search_options") = LineSearchOptions<T>());
+  }
+
+
   void bind_ice_shelf(py::module& module)
   {
     module.def("rate_factor", &icepack::rate_factor,
                "Compute the rate factor `A` in Glen's flow law");
 
-    py::class_<icepack::MembraneStress>(module, "MembraneStress")
+    py::class_<MembraneStress>(module, "MembraneStress")
       .def(py::init<const double>(),
            py::arg("n") = 3.0);
 
-    py::class_<icepack::Viscosity>(module, "Viscosity")
-      .def(py::init<const icepack::MembraneStress&>(),
+    py::class_<Viscosity>(module, "Viscosity")
+      .def(py::init<const MembraneStress&>(),
            py::keep_alive<1, 2>(),
-           py::arg("membrane_stress") = icepack::MembraneStress());
+           py::arg("membrane_stress") = MembraneStress());
 
-    py::class_<icepack::IceShelf>(module, "IceShelf")
-      .def(py::init<const std::set<dealii::types::boundary_id>&, const icepack::Viscosity&, const double>(),
+    bind_newton_search_options<VectorField<2>>(module, "VectorField2");
+
+    using boundary_ids = std::set<dealii::types::boundary_id>;
+
+    py::class_<IceShelf>(module, "IceShelf")
+      .def(py::init<const boundary_ids&, const Viscosity&, const double>(),
            "Create an object for modelling the flow of floating ice shelves",
            py::keep_alive<1, 3>(),
-           py::arg("dirichlet_boundary_ids") = std::set<dealii::types::boundary_id>{0},
-           py::arg("viscosity") = icepack::Viscosity(icepack::MembraneStress()),
+           py::arg("dirichlet_boundary_ids") = boundary_ids{0},
+           py::arg("viscosity") = Viscosity(MembraneStress()),
            py::arg("convergence_tolerance") = 1.0e-6)
-      .def("solve", &icepack::IceShelf::solve);
+      .def("solve", &IceShelf::solve,
+           "Solve the diagnostic equations for the ice velocity",
+           py::arg("thickness"),
+           py::arg("temperature"),
+           py::arg("boundary_velocity"),
+           py::arg("solve_options") = IceShelf::SolveOptions());
   }
 }
 
