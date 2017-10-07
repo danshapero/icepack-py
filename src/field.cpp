37ea7a20b4f0dfaa0fd4a51fd3a2016e6e2bab26
danshapero <shapero.daniel@gmail.com>
2017-10-07 08:31:03 -0700

    Using templates for binding field and vector field

--- src/field.cpp
+++ src/field.cpp
@@ -10,6 +10,23 @@ using icepack::VectorField;
 
 namespace icepack
 {
+  template <int rank>
+  void bind_field(py::module& module, const char * name)
+  {
+    using F = FieldType<rank, 2, primal>;
+    py::class_<FieldType<rank, 2, primal>>(module, name)
+      .def(py::init<std::shared_ptr<const Discretization<2>>>())
+      .def_property_readonly("discretization", &F::discretization)
+      .def("write_ucd",
+           [](const F& u, py::object& file)
+           {
+             std::ostringstream stream;
+             u.write_ucd("u", stream);
+             py::object write = file.attr("write");
+             write(stream.str());
+           });
+  }
+
   void bind_field(py::module& module)
   {
     module.def("read_msh", &icepack::read_msh,
@@ -21,29 +38,8 @@ namespace icepack
     module.def("make_discretization", &icepack::make_discretization<2>,
                py::keep_alive<0, 1>());
 
-    py::class_<Field<2>>(module, "Field2")
-      .def(py::init<std::shared_ptr<const Discretization<2>>>())
-      .def_property_readonly("discretization", &Field<2>::discretization)
-      .def("write_ucd",
-           [](const Field<2>& u, py::object& file)
-           {
-             std::ostringstream stream;
-             u.write_ucd("u", stream);
-             py::object write = file.attr("write");
-             write(stream.str());
-           });
-
-    py::class_<VectorField<2>>(module, "VectorField2")
-      .def(py::init<std::shared_ptr<const Discretization<2>>>())
-      .def_property_readonly("discretization", &VectorField<2>::discretization)
-      .def("write_ucd",
-           [](const VectorField<2>& u, py::object& file)
-           {
-             std::ostringstream stream;
-             u.write_ucd("u", stream);
-             py::object write = file.attr("write");
-             write(stream.str());
-           });
+    bind_field<0>(module, "Field2");
+    bind_field<1>(module, "VectorField2");
   }
 }
 
