eaa4cb5bfbb6c7115f66e6ea6d945babe355e9bb
danshapero <shapero.daniel@gmail.com>
2017-10-29 15:06:40 -0700

    Added binding for prognostic solve method of IceShelf

--- src/ice_shelf.cpp
+++ src/ice_shelf.cpp
@@ -49,18 +49,28 @@ namespace icepack
 
     using boundary_ids = std::set<dealii::types::boundary_id>;
 
+    VectorField<2> (IceShelf::*diagnostic_solve)(const Field<2>&, const Field<2>&, const VectorField<2>&, const IceShelf::SolveOptions) const = &IceShelf::solve;
+    Field<2> (IceShelf::*prognostic_solve)(const double dt, const Field<2>&, const Field<2>&, const VectorField<2>&, const Field<2>&) const = &IceShelf::solve;
+
     py::class_<IceShelf>(module, "IceShelf")
       .def(py::init<const boundary_ids&, const Viscosity&, const double>(),
            "Create an object for modelling the flow of floating ice shelves",
            py::arg("dirichlet_boundary_ids") = boundary_ids{0},
            py::arg("viscosity") = Viscosity(MembraneStress()),
            py::arg("convergence_tolerance") = 1.0e-6)
-      .def("solve", &IceShelf::solve,
+      .def("solve", diagnostic_solve,
            "Solve the diagnostic equations for the ice velocity",
            py::arg("thickness"),
            py::arg("temperature"),
            py::arg("boundary_velocity"),
-           py::arg("solve_options") = IceShelf::SolveOptions());
+           py::arg("solve_options") = IceShelf::SolveOptions())
+      .def("solve", prognostic_solve,
+           "Solve the prognostic equations for the ice thickness",
+           py::arg("dt"),
+           py::arg("thickness"),
+           py::arg("accumulation"),
+           py::arg("velocity"),
+           py::arg("inflow_thickness"));
   }
 }
 
